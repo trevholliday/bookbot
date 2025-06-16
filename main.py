cb795090026b49c0c9e459229dd485a8d77b9546
trevholliday <tholliday212@gmail.com>
2025-06-16 09:26:24 -0600

    setup report output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count
 from stats import get_chars_dict
+from stats import get_sorted_chars_dicts
 
 def get_book_text(file_path):
    file_contents = None
@@ -14,9 +15,22 @@ def main():
 
    word_count = get_word_count(content)
    chars_dict = get_chars_dict(content)
+   sorted_chars_dicts = get_sorted_chars_dicts(chars_dict)
 
-   print(f"{word_count} words found in the document")
-   print(chars_dict)
+   print("============ BOOKBOT ============")
+   print("Analyzing book found at books/frankenstein.txt...")
+   print("----------- Word Count ----------")
+   print(f"Found {word_count} total words")
+   print("--------- Character Count -------")
+   
+   for dict in sorted_chars_dicts:
+      char = dict["char"]
+      count = dict["num"]
+
+      if char.isalpha():
+         print(f"{char}: {count}")
+   
+   print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,17 @@ def get_chars_dict(book_content):
          charcter_count[lowercase] = 1
       
    return charcter_count
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_chars_dicts(chars_dict):
+   sorted_chars_list = []
+
+   for char in chars_dict:
+      sorted_chars_list.append({"char": char, "num": chars_dict[char]})
+
+   sorted_chars_list.sort(reverse=True, key=sort_on) 
+   return sorted_chars_list
