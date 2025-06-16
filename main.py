64ee9e3ba5b4f59d2484888cf127dfa153973ff2
trevholliday <tholliday212@gmail.com>
2025-06-16 08:51:08 -0600

    get_book_text start

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+   file_contents = None
+   
+   with open(file_path) as f:
+      file_contents = f.read()
+   
+   return file_contents
+
+
+def main():
+   content = get_book_text("books/frankenstein.txt")
+   print(content)
+
+
+main()
